cbe6cbe324b60ba4290b0c22af4b09aa4ff7e1d3
piratheepankumanan <piratheepankumanan@gmail.com>
2024-08-04 15:42:18 +1200

    Finished class functions to read episodes.csv and podcasts.csv

--- podcast/adapters/datareader/csvdatareader.py
+++ podcast/adapters/datareader/csvdatareader.py
@@ -23,21 +23,23 @@ class CSVDataReader:
         # return everything except first, first row is just identifiers
         return info_list[1:]
 
-    def get_podcast(self):
+    def get_podcastcsv(self):
         file_path = Path(__file__).resolve().parents[1] / 'data' / 'podcasts.csv'
         info_list = self.csv_read(file_path)
 
+        podcast_list = []
         for row in info_list:
-            pass
+            # row[0] = id, row[1] = title, row[2] = image, row[3] = description, row[4] = language, row[5] = categories, row[6] = website, row[7] = author, row[8] = itunes_id
+            podcast_list.append([row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8]])
+        return podcast_list
 
-    def get_episode(self):
+    def get_episodecsv(self):
         file_path = Path(__file__).resolve().parents[1] / 'data' / 'episodes.csv'
         info_list = self.csv_read(file_path)
 
+        episode_list = []
         for row in info_list:
-            pass
-
-
-
-
+            # row[0] = id, row[1] = podcast_id, row[2] = title, row[3] = audio, row[4] = audio_length, row[5] = description, row[6] = pubdate
+            episode_list.append([row[0], row[1], row[2], row[3], row[4], row[5], row[6]])
+        return episode_list
 
