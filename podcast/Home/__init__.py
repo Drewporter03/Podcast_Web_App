9b1855faa5c5514436fc6d031d2ab8b9a16144d7
Justin <66831913+stuutzer@users.noreply.github.com>
2024-08-09 23:32:47 +1200

    Small changes to various home and root init.py files

--- podcast/Home/__init__.py
+++ podcast/Home/__init__.py
@@ -1,11 +1,7 @@
-from flask import Blueprint, render_template, redirect
+from flask import Blueprint, render_template
 
 home_bp = Blueprint('home_bp', __name__, template_folder='templates')
 
 @home_bp.route('/home')
 def home():
     return render_template('home.html')
-
-@home_bp.route('/')
-def redirect_internal():
-    return redirect("/home")
