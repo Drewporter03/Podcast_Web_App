479a8e3e5155bdc594ad4033fff71cfc5b3db936
Justin <66831913+stuutzer@users.noreply.github.com>
2024-08-17 01:15:59 +1200

    Added pagination to podcast page

--- podcast/podcasts/podcasts.py
+++ podcast/podcasts/podcasts.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from podcast.adapters.datareader.csvdatareader import CSVDataReader
 
 
@@ -13,5 +13,12 @@ podcasts_bp = Blueprint('podcasts_bp', __name__, template_folder='templates')
 
 @podcasts_bp.route('/podcasts')
 def podcasts():
-    list_of_podcasts = create_podcasts()
-    return render_template('main.html', content_right='podcasts.html', podcasts=list_of_podcasts)
+    if request.args:
+        page = int(request.args.get('page'))
+        list_of_podcasts = create_podcasts()[page * 6 - 6: page * 6]
+        start, stop = page - 3, page + 3
+    else:
+        list_of_podcasts = create_podcasts()[:6]
+        start, stop = 1, 7
+
+    return render_template('main.html', content_right='podcasts.html', podcasts=list_of_podcasts, start = start, stop = stop)
