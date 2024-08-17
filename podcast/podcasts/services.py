f19e35e3a252aa5dc4af5094652716d6cb75964d
Drewporter03 <99791836+Drewporter03@users.noreply.github.com>
2024-08-17 20:42:51 +1200

    index from zero

--- podcast/podcasts/services.py
+++ podcast/podcasts/services.py
@@ -4,7 +4,7 @@ from podcast.adapters.repository import AbstractRepository
 
 def get_podcasts(repo: AbstractRepository):
     list_of_podcasts = []
-    for i in range(1, 1000):
+    for i in range(0, 1000):
         podcast = repo.get_podcast(i)
         list_of_podcasts.append(podcast)
     return list_of_podcasts
