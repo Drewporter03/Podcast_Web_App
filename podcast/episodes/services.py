6c6cfaadadb462cbc702fa130089dfe223bd9383
Drewporter03 <99791836+Drewporter03@users.noreply.github.com>
2024-08-17 21:11:13 +1200

    added episodes to episodes page

--- podcast/episodes/services.py
+++ podcast/episodes/services.py
@@ -1,11 +1,17 @@
-import podcast.adapters.repository as repo
-from podcast.adapters.memory_repository import MemoryRepository, populate
 from podcast.adapters.repository import AbstractRepository
+
 
 def get_podcasts(repo: AbstractRepository):
     list_of_podcasts = []
-    for i in range(0, 1000):
+    for i in range(1, 1000):
         podcast = repo.get_podcast(i)
         list_of_podcasts.append(podcast)
     return list_of_podcasts
 
+
+def get_episodes(repo: AbstractRepository):
+    list_of_episodes = []
+    for i in range(5633):
+        episode = repo.get_episode(i)
+        list_of_episodes.append(episode)
+    return list_of_episodes
