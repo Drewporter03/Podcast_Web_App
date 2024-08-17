07c9fbd1da2b45c983db8013c8aa289edd7eeda6
Drewporter03 <99791836+Drewporter03@users.noreply.github.com>
2024-08-17 20:40:33 +1200

    passing podcasts to episodes page

--- podcast/episodes/episode.py
+++ podcast/episodes/episode.py
@@ -1,14 +1,15 @@
 from flask import Blueprint, render_template, request
 import podcast.episodes.services as services
 import podcast.adapters.repository as repo
-
+from podcast.episodes.services import get_podcasts
 
 episodes_bp = Blueprint('episode_bp', __name__, template_folder='templates')
 
 
 @episodes_bp.route('/episodes')
 def episodes():
-    if request.args:
-        podcast_id = (request.args.get('podcast_id'))
-    list_of_podcasts = services.get_podcasts(repo.repository)
-    return render_template('main.html', content_right='episodes.html', podcast=podcast_id, podcasts=list_of_podcasts)
+    list_of_podcasts = get_podcasts(repo.repository)
+    podcast_id = request.args.get('podcast_id', default=1, type=int)
+
+    podcast = list_of_podcasts[podcast_id]
+    return render_template('main.html', content_right='episodes.html', podcast=podcast)
