143647ab9fda9ab37690c3647e741945c4a62543
Justin <66831913+stuutzer@users.noreply.github.com>
2024-08-20 22:22:40 +1200

    Changed formatting of episodes page

--- podcast/episodes/episode.py
+++ podcast/episodes/episode.py
@@ -10,23 +10,23 @@ episodes_bp = Blueprint('episode_bp', __name__, template_folder='templates')
 def episodes():
     list_of_podcasts = get_podcasts(repo.repository)
 
-    podcast_id = request.args.get('podcast_id', default=1, type=int)
-
+    podcast_id = request.args.get('podcast_id', type=int)
+    print(request.args)
     podcast = list_of_podcasts[podcast_id]
 
     if request.args.get('page'):
         page = int(request.args.get('page'))
-        list_of_episodes = get_episodes(repo.repository, podcast_id)[:6]
+        list_of_episodes = get_episodes(repo.repository, podcast_id)[page * 6 - 6: page * 6]
         if page <= 4:
             start = 1
             stop = 8
         else:
             start = page - 3
-            stop = page + 3
+            stop = page + 4 if page + 3 < 167 else 168
     else:
-        list_of_episodes = get_episodes(repo.repository, podcast_id)[1:7]
+        list_of_episodes = get_episodes(repo.repository, podcast_id)[:6]
+        start, stop = 1, 8
+        page = 1
 
-        start, stop = 1, 8    
 
-
-    return render_template('main.html', content_right='episodes.html', podcast=podcast, episodes=list_of_episodes)
+    return render_template('main.html', content_right='episodes.html', podcast=podcast, podcast_id = podcast_id, episodes=list_of_episodes, start = start, stop = stop, page=page)
