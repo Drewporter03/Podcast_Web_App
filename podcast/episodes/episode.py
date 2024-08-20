ceabc674b4e8e68b1d6cb61b87974c093edc4240
Drewporter03 <99791836+Drewporter03@users.noreply.github.com>
2024-08-20 23:30:22 +1200

    added dynamic stop and start to episodes pagination

--- podcast/episodes/episode.py
+++ podcast/episodes/episode.py
@@ -11,6 +11,15 @@ def episodes():
     list_of_podcasts = get_podcasts(repo.repository)
 
     podcast_id = request.args.get('podcast_id', type=int)
+    list_of_episodes = get_episodes(repo.repository, podcast_id)
+
+    count = 0
+    for episode in list_of_episodes:
+        if episode.podcast_id == podcast_id:
+            count += 1
+
+    max_pages = -(count//-6)
+
     print(request.args)
     podcast = list_of_podcasts[podcast_id]
 
@@ -19,14 +28,15 @@ def episodes():
         list_of_episodes = get_episodes(repo.repository, podcast_id)[page * 6 - 6: page * 6]
         if page <= 4:
             start = 1
-            stop = 8
+            stop = 8 if max_pages > 8 else max_pages +1
         else:
             start = page - 3
-            stop = page + 4 if page + 3 < 167 else 168
+            stop = max_pages + 1 if start + 8 > max_pages else start + 7
     else:
         list_of_episodes = get_episodes(repo.repository, podcast_id)[:6]
-        start, stop = 1, 8
+        start = 1
+        stop = max_pages + 1 if start + 8 > max_pages else start + 7
         page = 1
 
-
-    return render_template('main.html', content_right='episodes.html', podcast=podcast, podcast_id = podcast_id, episodes=list_of_episodes, start = start, stop = stop, page=page)
+    return render_template('main.html', content_right='episodes.html', podcast=podcast, podcast_id=podcast_id,
+                           episodes=list_of_episodes, start=start, stop=stop, page=page, max_page = max_pages)
