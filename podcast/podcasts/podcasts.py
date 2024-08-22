54e02d1b3c5bf422a4b51b559600af128d8dc06e
Drewporter03 <99791836+Drewporter03@users.noreply.github.com>
2024-08-22 12:44:57 +1200

    removed some magic nums

--- podcast/podcasts/podcasts.py
+++ podcast/podcasts/podcasts.py
@@ -1,4 +1,6 @@
 from flask import Blueprint, render_template, request
+
+import podcast
 from podcast.adapters.datareader.csvdatareader import CSVDataReader
 import podcast.podcasts.services as services
 import podcast.adapters.repository as repo
@@ -14,7 +16,10 @@ podcasts_bp = Blueprint('podcasts_bp', __name__, template_folder='templates')
 @podcasts_bp.route('/podcasts')
 def podcasts():
 
-    max_pages = 167
+    number_of_episodes = len(services.get_podcasts(repo.repository))
+    max_pages = -(number_of_episodes//-6)
+
+    #max_pages = 167
 
     if request.args:
         page = int(request.args.get('page'))
@@ -24,10 +29,10 @@ def podcasts():
             stop = 8
         else:
             start = page - 3
-            stop = page + 4 if page + 3 < 167 else 168
-            if page + 3 > 167:
-                stop = 167 + 1
-                start = 167 - 7
+            stop = page + 4 if page + 3 < max_pages else max_pages + 1
+            if page + 3 > max_pages:
+                stop = max_pages + 1
+                start = max_pages - 7
             else:
                 start = page - 3
                 stop = start + 7
@@ -36,4 +41,4 @@ def podcasts():
         start, stop = 1, 8
         page = 1
 
-    return render_template('main.html', content_right='podcasts.html', podcasts=list_of_podcasts, start = start, stop = stop, page=page, max_pages = 167)
+    return render_template('main.html', content_right='podcasts.html', podcasts=list_of_podcasts, start = start, stop = stop, page=page, max_pages = max_pages)
