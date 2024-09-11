1023f2e7340bef1899e22de4fcbc61cf84b7a0f0
Drewporter03 <99791836+Drewporter03@users.noreply.github.com>
2024-09-11 22:03:15 +1200

    made changes to comply with SOLID, also multiple episodes now show up in search

--- podcast/podcasts/podcasts.py
+++ podcast/podcasts/podcasts.py
@@ -7,40 +7,39 @@ import podcast.adapters.repository as repo
 from podcast.adapters.memory_repository import MemoryRepository, populate
 from podcast.adapters.repository import AbstractRepository
 
-
-
-
 podcasts_bp = Blueprint('podcasts_bp', __name__, template_folder='templates')
+
+
+def filter_podcasts(podcasts, query, parameter):
+    searched_podcasts = []
+    for podcast in podcasts:
+        if parameter == "title" and query in podcast.title:
+            searched_podcasts.append(podcast)
+        elif parameter == "author" and query in podcast.author.name:
+            searched_podcasts.append(podcast)
+        elif parameter == "category" and query in podcast.categories[0].name:
+            searched_podcasts.append(podcast)
+
+    return searched_podcasts
 
 
 @podcasts_bp.route('/podcasts')
 def podcasts():
-
     number_of_episodes = len(services.get_podcasts(repo.repository))
-    max_pages = int(round(number_of_episodes/10))
+    max_pages = int(round(number_of_episodes / 10))
 
     if request.args:
         query = request.args.get('q')
         parameter = request.args.get('p')
-        print(parameter)
         page = int(request.args.get('page', default=1, type=int))
         list_of_podcasts = services.sorted_podcasts_by_title(repo.repository)[page * 10 - 10: page * 10]
         if query is not None:
-            filtered_list_of_podcasts = []
-            if parameter == "title":
-                for podcast in list_of_podcasts:
-                    if query in podcast.title:
-                        filtered_list_of_podcasts.append(podcast)
-            if parameter == "author":
-                for podcast in list_of_podcasts:
-                    if query in podcast.author.name:
-                        filtered_list_of_podcasts.append(podcast)
-            if parameter == "category":
-                for podcast in list_of_podcasts:
-                    print(podcast.categories[0].name)
-                    if query in podcast.categories[0].name:
-                        filtered_list_of_podcasts.append(podcast)
-            list_of_podcasts = filtered_list_of_podcasts
+            list_of_podcasts = filter_podcasts(services.sorted_podcasts_by_title(repo.repository), query, parameter)
+            number_of_episodes = len(list_of_podcasts)
+            max_pages = (round(number_of_episodes / 10))
+            list_of_podcasts = list_of_podcasts[page * 10 - 10: page * 10]
+
+
         if page <= 4:
             start = 1
             stop = 8
@@ -58,4 +57,5 @@ def podcasts():
         start, stop = 1, 8
         page = 1
 
-    return render_template('main.html', content_right='podcasts.html', podcasts=list_of_podcasts, start = start, stop = stop, page=page, max_pages = max_pages)
+    return render_template('main.html', content_right='podcasts.html', podcasts=list_of_podcasts, start=start,
+                           stop=stop, page=page, max_pages=max_pages)
