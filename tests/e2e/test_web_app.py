4839bc4e0b9cf35d9e7ea3082924320a89487c58
piratheepankumanan <piratheepankumanan@gmail.com>
2024-09-11 17:04:00 +1200

    added test case function to test register page

--- tests/e2e/test_web_app.py
+++ tests/e2e/test_web_app.py
@@ -1,11 +1,14 @@
 from tests.conftest import client
+import pytest
+from flask import session
 
 
 # Test that the home page returns the welcome message
 def test_home(client):
     response = client.get('/home')
     assert response.status_code == 200
-    assert b'<h1>Welcome to <a href="home"><span class="logo">mixcast.<'b'/span></a></h1>' in response.data
+
+    assert b'<h1>Welcome to <a href="home" class="highlight logo">mixcast.</a></h1>' in response.data
 
 
 # Test that the podcasts page returns the header 'Podcasts' and the first episode "#better"
@@ -22,3 +25,11 @@ def test_episodes_page(client):
     assert response.status_code == 200
     assert b'Bethel Presbyterian Church (EPC) Sermons' in response.data
     assert b'Believing the Impossible (Luke 1:26-45)' in response.data
+
+
+def test_register_page(client):
+    response = client.get('/register').status_code
+    assert response == 200
+    response_data = client.post('/register', data={
+        'user_name': 'kumanan', 'password': 'BigManTing1'})
+    assert response_data.headers['Location'] == '/login'
