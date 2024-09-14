0e3b1049a1ac45318c8da0786c98b0fea49cfa7c
Drewporter03 <99791836+Drewporter03@users.noreply.github.com>
2024-09-14 18:24:52 +1200

    add and remove function for Reviews - started to implement reviews for podcasts

--- podcast/episodes/episode.py
+++ podcast/episodes/episode.py
@@ -8,10 +8,15 @@ episodes_bp = Blueprint('episode_bp', __name__, template_folder='templates')
 
 @episodes_bp.route('/episodes')
 def episodes():
+
+
+
     list_of_podcasts = get_podcasts(repo.repository)
 
     podcast_id = request.args.get('podcast_id', type=int)
     list_of_episodes = services.sorted_episodes_by_date(repo.repository, podcast_id)
+
+    reviews = services.get_podcast_reviews(podcast_id, repo.repository)
 
     count = 0
     for episode in list_of_episodes:
@@ -43,4 +48,4 @@ def episodes():
         page = 1
 
     return render_template('main.html', content_right='episodes.html', podcast=podcast, podcast_id=podcast_id,
-                           episodes=list_of_episodes, start=start, stop=stop, page=page, max_page = max_pages)
+                           episodes=list_of_episodes, start=start, stop=stop, page=page, max_page = max_pages, reviews = reviews)
--- podcast/episodes/services.py
+++ podcast/episodes/services.py
@@ -1,10 +1,15 @@
 from podcast.adapters.repository import AbstractRepository
 from podcast.domainmodel.model import Author, Podcast, Category, User, PodcastSubscription, Episode, Playlist, Review
 import datetime
+
+
 class NonExistentPodcastException(Exception):
     pass
+
+
 class UnknownUserException(Exception):
     pass
+
 
 def get_podcasts(repo: AbstractRepository):
     list_of_podcasts = []
@@ -22,26 +27,49 @@ def get_episodes(repo: AbstractRepository, podcast_id):
             list_of_episodes.append(episode)
     return list_of_episodes
 
+
 def sorted_episodes_by_date(repo: AbstractRepository, podcast_id):
     list_of_episodes = get_episodes(repo, podcast_id)
     return sorted(list_of_episodes, key=lambda episode: episode.date1)
 
+
 def get_podcast_reviews(podcast_id, repo: AbstractRepository):
     podcast = repo.get_podcast(podcast_id)
-    if podcast == None:
+    if podcast is None:
         raise NonExistentPodcastException
+
     return podcast.reviews
+
+def get_average_reviews(podcast_id, repo: AbstractRepository):
+    reviews = get_podcast_reviews(podcast_id, repo)
+    if len(reviews) == 0:
+        average = "N/A"
+        return average
+    average = 0
+    for review in reviews:
+        average += review.rating
+        average /= len(reviews)
+
+    return average
+
+
+
+
+
+
+
+
+
 
 
 def add_reviews(podcast_id, review_txt: str, review_rating: int, user_name: str, repo: AbstractRepository):
     podcast = repo.get_podcast(podcast_id)
-    if podcast != None:
+    if podcast is not None:
         user = repo.get_user(user_name)
-        if user != None:
+        if user is not None:
             review = Review(id(user), user, podcast, review_rating, review_txt)
             repo.add_review(review)
         else:
             raise UnknownUserException
     else:
         raise NonExistentPodcastException
-
