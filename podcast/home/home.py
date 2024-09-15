9bd2339f1fdc68dac3fb7df9c371aff995f819fd
Justin <66831913+stuutzer@users.noreply.github.com>
2024-09-15 19:00:02 +1200

    Added styling to login page

--- podcast/home/home.py
+++ podcast/home/home.py
@@ -1,8 +1,13 @@
 from flask import Blueprint, render_template
+import podcast.adapters.repository as repo
+import podcast.podcasts.services as services
 
 home_bp = Blueprint('home_bp', __name__, template_folder='templates')
 
 
 @home_bp.route('/home')
 def home():
-    return render_template('main.html', content_right = "home.html")
+    list_of_podcasts = services.sorted_podcasts_by_title(repo.repository)
+
+
+    return render_template('main.html', content_right = "home.html", podcasts=list_of_podcasts)
