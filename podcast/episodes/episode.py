fb13b03fa572911167a88bf81dfecf2c0aa6283a
Justin <66831913+stuutzer@users.noreply.github.com>
2024-09-15 12:19:43 +1200

    Alterations to episode page behaviour depending on login state

--- podcast/episodes/episode.py
+++ podcast/episodes/episode.py
@@ -29,30 +29,12 @@ def episodes():
         if episode.podcast_id == podcast_id:
             count += 1
 
-    max_pages = (count // 10)
-
     podcast = list_of_podcasts[podcast_id]
 
     if request.args.get('page'):
         page = int(request.args.get('page'))
-        list_of_episodes = services.sorted_episodes_by_date(repo.repository, podcast_id)[page * 10 - 10: page * 10]
-        if page <= 4:
-            start = 1
-            stop = 8 if max_pages > 8 else max_pages + 1
-        else:
-            if page + 3 > max_pages:
-                stop = max_pages + 1
-                start = max_pages - 7
-            else:
-                start = page - 3
-                stop = start + 7
-    else:
-        list_of_episodes = services.sorted_episodes_by_date(repo.repository, podcast_id)[:10]
-        start = 1
-        stop = max_pages + 1 if start + 8 > max_pages else start + 7
-        page = 1
+        list_of_episodes = services.sorted_episodes_by_date(repo.repository, podcast_id)
 
     return render_template('main.html', content_right='episodes.html', podcast=podcast, podcast_id=podcast_id,
-                           episodes=list_of_episodes, start=start, stop=stop, page=page, max_page=max_pages,
-                           reviews=reviews, average = average)
+                           episodes=list_of_episodes, reviews=reviews, average = average)
 
