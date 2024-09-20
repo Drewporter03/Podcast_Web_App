59d41574027301ea1c76bd9661eb6d18584ca3eb
Drewporter03 <99791836+Drewporter03@users.noreply.github.com>
2024-09-20 22:50:56 +1200

    moved functions to service layer

--- podcast/podcasts/podcasts.py
+++ podcast/podcasts/podcasts.py
@@ -12,45 +12,11 @@ import podcast.episodes.services as episodes_services
 podcasts_bp = Blueprint('podcasts_bp', __name__, template_folder='templates')
 
 
-def filter_podcasts(podcasts, query, parameter):
-    searched_podcasts = []
-    for podcast in podcasts:
-        if parameter == "title" and query in podcast.title:
-            searched_podcasts.append(podcast)
-        elif parameter == "author" and query in podcast.author.name:
-            searched_podcasts.append(podcast)
-        elif parameter == "category" and query in podcast.categories[0].name:
-            searched_podcasts.append(podcast)
-
-    return searched_podcasts
-
-
-def calculate_pagination(page, max_pages, list_of_podcasts):
-    if page <= 4:
-        start = 1
-        stop = 8 if max_pages > 8 else max_pages + 1
-    else:
-        if page + 3 > max_pages:
-            stop = max_pages + 1
-            start = 1 if max_pages - 7 <= 1 else max_pages - 7
-        else:
-            start = page - 3
-            stop = start + 7
-    list_of_podcasts = list_of_podcasts[page * 10 - 10: page * 10]
-
-    return start, stop, list_of_podcasts
-
-
-def calculate_pages(list_of_podcasts):
-    number_of_episodes = len(list_of_podcasts)
-    return int(round(number_of_episodes / 10))
-
-
 @podcasts_bp.route('/podcasts', methods=['POST', 'GET'])
 def podcasts():
     list_of_podcasts = services.sorted_podcasts_by_title(repo.repository)
     list_of_episodes = services.get_episodes(repo.repository)
-    max_pages = calculate_pages(list_of_podcasts)
+    max_pages = services.calculate_pages(list_of_podcasts)
 
     if request.args:
         query = request.args.get('q')
@@ -58,14 +24,14 @@ def podcasts():
         page = (request.args.get('page', default=1, type=int))
 
         if query is not None:
-            list_of_podcasts = filter_podcasts(list_of_podcasts, query, parameter)
-            max_pages = calculate_pages(list_of_podcasts)
+            list_of_podcasts = services.filter_podcasts(list_of_podcasts, query, parameter)
+            max_pages = services.calculate_pages(list_of_podcasts)
 
-        start, stop, list_of_podcasts = calculate_pagination(page, max_pages, list_of_podcasts)
+        start, stop, list_of_podcasts = services.calculate_pagination(page, max_pages, list_of_podcasts)
 
     else:
         page = 1
-        start, stop, list_of_podcasts = calculate_pagination(page, max_pages, list_of_podcasts)
+        start, stop, list_of_podcasts = services.calculate_pagination(page, max_pages, list_of_podcasts)
 
     playlist_form = playlistForm()
     if playlist_form.validate_on_submit():
@@ -84,17 +50,13 @@ def podcasts():
             playlist_services.add_podcast(repo.repository, 0, podcast_id)
 
     if 'user_name' in session:
-        try:
-            playlist_episodes = playlist_services.get_user_playlist(repo.repository, 0).podcast_list
-        except playlist_services.PlaylistNotFoundException:
-            user_name = session['user_name']
-            playlist_services.add_playlist(repo.repository, user_name, f"{user_name}'s Playlist")
-            playlist_episodes = playlist_services.get_user_playlist(repo.repository, 0).podcast_list
+        playlist_episodes = playlist_services.get_user_playlist(repo.repository, 0).podcast_list
     else:
         playlist_episodes = None
 
     return render_template('main.html', content_right='podcasts.html', podcasts=list_of_podcasts, start=start,
-                           stop=stop, page=page, max_pages=max_pages, playlist_form=playlist_form, playlist_episodes=playlist_episodes, list_of_episodes=list_of_episodes)
+                           stop=stop, page=page, max_pages=max_pages, playlist_form=playlist_form,
+                           playlist_episodes=playlist_episodes, list_of_episodes=list_of_episodes)
 
 
 class playlistForm(FlaskForm):
--- podcast/podcasts/services.py
+++ podcast/podcasts/services.py
@@ -19,3 +19,36 @@ def get_podcasts(repo: AbstractRepository):
 def sorted_podcasts_by_title(repo: AbstractRepository):
     list_of_podcasts = get_podcasts(repo)
     return sorted(list_of_podcasts, key=lambda podcast: podcast.title)
+
+
+def filter_podcasts(podcasts, query, parameter):
+    searched_podcasts = []
+    for podcast in podcasts:
+        if parameter == "title" and query in podcast.title:
+            searched_podcasts.append(podcast)
+        elif parameter == "author" and query in podcast.author.name:
+            searched_podcasts.append(podcast)
+        elif parameter == "category" and query in podcast.categories[0].name:
+            searched_podcasts.append(podcast)
+
+    return searched_podcasts
+
+
+def calculate_pagination(page, max_pages, list_of_podcasts):
+    if page <= 4:
+        start = 1
+        stop = 8 if max_pages > 8 else max_pages + 1
+    else:
+        if page + 3 > max_pages:
+            stop = max_pages + 1
+            start = 1 if max_pages - 7 <= 1 else max_pages - 7
+        else:
+            start = page - 3
+            stop = start + 7
+    list_of_podcasts = list_of_podcasts[page * 10 - 10: page * 10]
+
+    return start, stop, list_of_podcasts
+
+def calculate_pages(list_of_podcasts):
+    number_of_episodes = len(list_of_podcasts)
+    return int(round(number_of_episodes / 10))
