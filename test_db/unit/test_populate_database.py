d439be3df995b56b8f41bc51cc5d0a9b160ed8d6
piratheepankumanan <piratheepankumanan@gmail.com>
2024-10-06 20:21:24 +1300

    created the first test case for repo_populate

--- test_db/unit/test_populate_database.py
+++ test_db/unit/test_populate_database.py
@@ -4,4 +4,4 @@ from podcast.adapters.orm import mapper_registry
 def test_database_populate_inspect_table_names(database_engine):
     # Get table information
     inspector = inspect(database_engine)
-    assert inspector.get_table_names() == ['article_tags', 'articles', 'comments', 'tags', 'users']
+    assert inspector.get_table_names() == ['authors', 'categories', 'episodes', 'playlist', 'playlist_episodes', 'podcast_categories', 'podcasts', 'reviews', 'users']
