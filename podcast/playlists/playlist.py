1e01a1f5f2185dca3db08d6919a1838a2f97c88b
Drewporter03 <99791836+Drewporter03@users.noreply.github.com>
2024-10-09 17:40:18 +1300

    fixed double click issue to remove episode

--- podcast/playlists/playlist.py
+++ podcast/playlists/playlist.py
@@ -14,23 +14,15 @@ def playlists():
     user_name = session['user_name']
     user = services.get_user(repo.repository, user_name)
     user_id = user.id
-    print(user_id)
     user_playlist = services.get_user_playlist(repo.repository, user_id)
-    # user_playlist = services.get_user_playlist(repo.repository, user_id)
-    # playlist_episodes = services.get_episodes(repo.repository, user_id)
-
-    #whoopsie
     playlist_episodes = user_playlist._episodes
-    print(playlist_episodes)
     remove_episode_from_playlist = RemoveEpisodeForm()
     if remove_episode_from_playlist.validate_on_submit():
-        if remove_episode_from_playlist.episode_id.data is not None:
-            services.remove_episode(repo.repository, user_id, remove_episode_from_playlist.episode_id.data)
+        services.remove_episode(repo.repository, user_id, remove_episode_from_playlist.episode_id.data)
 
-
-    return render_template('main.html', content_right='playlists.html', playlist_episodes=playlist_episodes, user_name=user_name,
+    return render_template('main.html', content_right='playlists.html', playlist_episodes=playlist_episodes,
+                           user_name=user_name,
                            remove_episode_from_playlist=remove_episode_from_playlist)
-
 
 
 class RemoveEpisodeForm(FlaskForm):
@@ -41,4 +33,3 @@ class RemoveEpisodeForm(FlaskForm):
 class RemovePodcastForm(FlaskForm):
     podcast_id = IntegerField()
     submit = SubmitField('−')
-
