591ff8d8c0a86bc9c5f546f938239de8724d463e
piratheepankumanan <piratheepankumanan@gmail.com>
2024-10-10 17:39:21 +1300

    added test case for adding multiple authors

--- tests_db/unit/test_database_repository.py
+++ tests_db/unit/test_database_repository.py
@@ -25,10 +25,25 @@ def test_add_and_get_playlist(session_factory):
     playlist2 = repo.get_playlist(playlist.id)
     assert playlist == playlist2
 
+# Test case to add and get author from database
 def test_add_and_get_author(session_factory):
     repo = SqlAlchemyRepository(session_factory)
-    author = Author(2, "Kumanan")
+    author = Author(1, "Kumanan")
     repo.add_author(author)
-    author2 = repo.get_author(2)
+    author2 = repo.get_author(1)
     assert author2 == author
 
+# Test case to adding multiple authors
+def test_add_and_multiple_author(session_factory):
+    repo = SqlAlchemyRepository(session_factory)
+    author = Author(7000, "Kumanan1")
+    author2 = Author(7001, "Kumanan2")
+    author3 = Author(7002, "Kumanan3")
+    author4 = Author(7003, "Kumanan4")
+    author_list1 = [author, author2, author3, author4]
+    repo.add_multiple_authors(author_list1)
+    author_list2 = []
+    for i in range(0, 4):
+        author_list2.append(repo.get_author(7000+i))
+    for j in range(len(author_list1)):
+        assert author_list1[j] == author_list2[j]
