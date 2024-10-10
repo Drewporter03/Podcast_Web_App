9b64dee19672ebe99b50813add7c5e4fc1b1233e
piratheepankumanan <piratheepankumanan@gmail.com>
2024-10-10 14:55:29 +1300

    added a test case to check what happens when a non existant user is called

--- tests_db/unit/test_database_repository.py
+++ tests_db/unit/test_database_repository.py
@@ -5,9 +5,14 @@ from podcast.adapters.database_repository import SqlAlchemyRepository
 # Test case to add and get user from database
 def test_repository_can_add_and_get_user(session_factory):
     repo = SqlAlchemyRepository(session_factory)
-
     user = User(1, 'Kumanan', 'Kumanan!1')
     repo.add_user(user)
     user2 = repo.get_user('Kumanan')
-
     assert user2 == user
+
+# Test case to check if retrieving non existant user worked as intended
+def test_repository_does_not_retrieve_a_non_existent_user(session_factory):
+    repo = SqlAlchemyRepository(session_factory)
+    user = repo.get_user('KumananisNotCool')
+    assert user is None
+
