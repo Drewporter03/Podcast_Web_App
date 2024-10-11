60b40b51a791db81935b1de79015f1ded46ee6b0
Justin <66831913+stuutzer@users.noreply.github.com>
2024-10-11 16:56:12 +1300

    Overhaul of homepage design and responsive design implementation for homepage, podcasts and settings

--- podcast/home/home.py
+++ podcast/home/home.py
@@ -1,13 +1,16 @@
 from flask import Blueprint, render_template
 import podcast.adapters.repository as repo
 import podcast.podcasts.services as services
+import random
+
 
 home_bp = Blueprint('home_bp', __name__, template_folder='templates')
 
 
 @home_bp.route('/home')
 def home():
-    list_of_podcasts = services.sorted_podcasts_by_title(repo.repository)
-
+    list_of_podcasts = []
+    for i in range(3):
+        list_of_podcasts.append(repo.repository.get_podcast(random.randrange(1,len(repo.repository.get_podcasts()) - 1)))
 
     return render_template('main.html', content_right = "home.html", podcasts=list_of_podcasts)
